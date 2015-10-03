bd2ff258aad3cb29e4c16066ee7576015541645e
Jayteare <jon.dubdub@yahoo.com>
2015-10-03 13:51:28 -0700

    Create Linked_List_Problem9.cpp

---
+++ Data_Structures/Linked_Lists/Linked_List_Problem9.cpp
@@ -0,0 +1,32 @@
+/*
+  Get Nth element from the end in a linked list of integers
+  Number of elements in the list will always be greater than N.
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+int GetNode(Node *head,int positionFromTail)
+{
+  // This is a "method-only" submission. 
+  // You only need to complete this method. 
+    int size = 0,c_ount = 0,dman = 0;
+    Node *current = new Node;
+    current = head;
+    while(current!=NULL)
+    {
+        current = current->next;
+        size++;
+    }
+    size -= positionFromTail;
+    current = head;
+    while(c_ount < size-1)
+    {
+        current = current->next;
+        c_ount++;
+    }
+    dman = current->data;
+    return dman;
+}
