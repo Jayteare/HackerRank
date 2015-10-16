c950aa04758820b7c3c326a9da6bb2c82c85d752
Jayteare <Jayteare@users.noreply.github.com>
2015-10-16 00:26:18 -0700

    Update BotClean_Stochastic.cpp

--- Artificial-Intelligence/Bot-Building/BotClean_Stochastic.cpp
+++ Artificial-Intelligence/Bot-Building/BotClean_Stochastic.cpp
@@ -4,84 +4,64 @@ using namespace std;
 //Recieved full 10.00 points ranked as first seed.
 void nextMove(int posr, int posc, vector <string> board) {
     //add logic here
-    //add logic here
     string grid_floor;
-    int bot_x = posr,bot_y = posc;
-    int clean_x =0,clean_y=0;
+    int bot_x   = posr , bot_y   = posc;
+    int clean_x = 0    , clean_y = 0;
 
-    bool check_x,check_y,check_r = false;
-    bool cleared_board = false;
+    bool check_x,check_y;
     int x_moves,y_moves;
     
-    //FIND THE PRINCESS NOW
-    grid_floor = board[0];
+    //For Loop - Focuses on locating the target 'd';
     for(int i =0; i < board.size();i++)
     {
-        grid_floor = board[i];
-        std::size_t found = grid_floor.find("d");
-        if (found!=std::string::npos)
+        grid_floor   = board[i]; //Assigns the row in the matrix grid;
+        size_t found = grid_floor.find("d"); //Checks the columns in the matrix grid;
+        if (found!=string::npos)
         {
             clean_x = i;
             clean_y = found;
 
+            //Calculates the amount of differences in (x,y) distance in 2D grid;
             x_moves = clean_x - bot_x;
             y_moves = clean_y - bot_y;
 
             if((x_moves == 0) && (y_moves == 0))
             {
                 cout << "CLEAN" << endl;
-                cleared_board = true;
-                break;
+                return;//Removes redundencies;
             }
             break;
         }       
     }  
     
-    if(cleared_board != true)
+    //Brute Force point-move checking;
+    if((x_moves == 0))
     {
-        if((x_moves == 0))
-        {
-                check_y = y_moves > 0;
-                if(check_y)
-                {
-                    cout << "RIGHT" << endl;        
-                }
-                else if(y_moves >1 && x_moves <0)
-                {
-                    cout << "DOWN" << endl;
-                }     
-                else
-                {
-                    cout << "LEFT" << endl;    
-                }
-        }
-
-        check_x = x_moves > 0;
-        if(check_x)
-        {
-            if((x_moves >= 1) && (y_moves == 0))
-            {
-                cout << "DOWN" << endl; 
-                check_r = true;  
-            }
-            else if ((x_moves >= 1) && (y_moves >= 1))
-            {
-                cout << "RIGHT" << endl; 
-                check_r = true;    
-            }
+        check_y = y_moves > 0;
+        if(check_y)
+            cout << "RIGHT" << endl;        
+        else if(y_moves >1 && x_moves <0)
+            cout << "DOWN" << endl;
             else
-            {
-                cout << "LEFT" << endl; 
-                check_r = true;        
-            }
+            cout << "LEFT" << endl;    
 
-        }
-        else if(x_moves < 0)
-        {
-            cout << "UP" << endl;
-            check_r = true;
-        }   
+            return;
     }
+
+    check_x = x_moves > 0;
+    //Brute Force point-move checking;
+    if(check_x)
+    {
+        if((x_moves >= 1) && (y_moves == 0))
+            cout << "DOWN" << endl; 
+        else if ((x_moves >= 1) && (y_moves >= 1))
+            cout << "RIGHT" << endl;
+            else
+            cout << "LEFT" << endl; 
+            return;
+    }
+    else if(x_moves < 0)
+        cout << "UP" << endl;
 
 }
 int main(void) {
