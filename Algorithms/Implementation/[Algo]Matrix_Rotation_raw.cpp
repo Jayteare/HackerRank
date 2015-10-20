4068fbcfb5114042c2161337ea57b48182109821
Jayteare <Jayteare@users.noreply.github.com>
2015-10-19 20:06:37 -0700

    Update [Algo]Matrix_Rotation_raw.cpp

--- Algorithms/Implementation/[Algo]Matrix_Rotation_raw.cpp
+++ Algorithms/Implementation/[Algo]Matrix_Rotation_raw.cpp
@@ -9,11 +9,11 @@ using namespace std;
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     int M,N,R,in_,temp,temp2;
-    int X = 0, Y = 0, inc = 1;
+    int X = 0, inc = 1;
     int N_counter = 0;
     cin >> M >> N >> R;
     
-    N_counter = N/2;
+    N_counter = 2;
     int arr[M][N];
     for(int i = 0; i < M ; i++)
     {
@@ -27,8 +27,7 @@ int main() {
     for(int index = 0; index < N_counter; index++)
     {
         for(int jndex = 0; jndex < R; jndex++)
-        {
-            
+        {            
             if( M > 2  && N > 2)
             {
                 //LEFT
@@ -44,33 +43,39 @@ int main() {
                     temp2 = arr[i+1][X];
                     arr[i+1][X] = temp;
                     temp = temp2; 
-                }            
+                }   
+                
+                
                 //BOTTOM
                 for(int j = 1+X; j < (N-1)-X; j++)
                 {
                     temp2 = arr[M-inc][j];
                     arr[M-inc][j] = temp;
                     temp = temp2; 
-                }          
+                }
+                
                 //RIGHT
-                for(int k = (M-1)-X; k > 0; k--)
+                for(int k = (M-1)-X; k > X; k--)
                 {
                     temp2 = arr[k][(N-1)-X];
                     arr[k][(N-1)-X] = temp;
                     temp = temp2;   
-                }           
+                }    
+                
+                
                 //TOP
-                for(int h = (N-1) - X-X-X; h > 0; h--)
+                for(int h = (N-1)-X; h > X; h--)
                 {
-                    temp2 = arr[X][h];
-                    arr[X][h-X] = temp;
+                    temp2 = arr[X][h-X];
+                    arr[X][h] = temp;
                     temp = temp2; 
-                }
-
+                }              
             }
+            
             temp2 = 0;
             temp  = 0;    
         }
+        
         X++;inc++;
     }
     
