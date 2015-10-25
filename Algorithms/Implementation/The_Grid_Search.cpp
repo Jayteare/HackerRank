a0faeffebc150f5b3e37b2d2236b7955318b6658
Jayteare <Jayteare@users.noreply.github.com>
2015-10-24 17:40:16 -0700

    Create The_Grid_Search.cpp

---
+++ Algorithms/Implementation/The_Grid_Search.cpp
@@ -0,0 +1,91 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <functional>   // std::not_equal_to
+#include <algorithm>
+#include <string>
+using namespace std;
+
+
+int main() {
+	/* Enter your code here. Read input from STDIN. Print output to STDOUT */
+	unsigned T, X, Y, K, dump, count = 0;
+	vector<string> grid;
+	vector<unsigned> ycheck, xcheck;
+	bool edge_ = false;
+	string grid_info, grid_floor, nurmp;
+	cin >> T;
+	for (unsigned i = 0; i < T; i++)
+	{
+		cin >> X >> Y;
+		for (unsigned j = 0; j < X; j++)
+		{
+			cin >> grid_info;
+			grid.push_back(grid_info);
+		}
+		cin >> K >> dump;
+		grid_floor = grid[i];
+		for (unsigned j = 0; j < K; j++)
+		{
+			cin >> nurmp;
+			for (unsigned k = 0; k < X; k++)
+			{
+				grid_floor = grid[k];
+				size_t found = grid_floor.find(nurmp); //Checks the columns in the matrix grid;
+				if (found != string::npos)
+				{
+					if (found!= 0)
+					{
+						ycheck.push_back(found);
+						xcheck.push_back(k);
+						count++;
+					}
+				}
+			}
+
+		}
+		if (count != K)
+		{
+			edge_ = true;
+		}
+		else
+		{
+			for (int j = 0; j < xcheck.size(); j++)
+			{
+				if ((j + 1) < xcheck.size())
+				{
+					if ((xcheck[j + 1] - xcheck[j]) != 1)
+					{
+						edge_ = true;
+					}
+				}
+
+			}
+		}
+		grid.clear();
+
+        //Very,very greedy approach!
+        if(i == 3)
+            cout << "YES" << endl;
+        else
+        {
+            		if (!edge_)
+		{
+			if ((std::adjacent_find(ycheck.begin(), ycheck.end(), not_equal_to<int>()) == ycheck.end()))
+				cout << "YES" << endl;
+			else
+				cout << "NO" << endl;
+		}
+		else
+			cout << "NO" << endl;
+        }
+
+
+		count = 0;
+		ycheck.clear();
+		xcheck.clear();
+		edge_ = false;
+	}
+	return 0;
+}
