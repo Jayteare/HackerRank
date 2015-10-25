7c73263bc5067b189b27ebbeac57e2555a287955
Jayteare <Jayteare@users.noreply.github.com>
2015-10-25 00:39:38 -0700

    Update and rename String_Reduction_raw.cpp to String_Reduction.cpp

---
+++ Algorithms/Dynamic-Programming/String_Reduction.cpp
@@ -0,0 +1,96 @@
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstdlib>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/* Head ends here */
+
+
+// * Check to see if all the letters in a string are the same (CHECK);
+// * Count the number of 'a'"s in the string (CHECK);
+bool check_same(string s)
+{
+    for(int i = 0; i<s.length(); i++)
+    {
+        if(s[i] != s[0])
+            return false;
+    }
+    return true;
+}
+//------------------------------------------
+int count_A(string s)
+{
+    int a = 0;
+    for(int i = 0; i < s.length(); i++)
+    {
+        if(s[i] == 'a')
+            a++;
+    }
+    
+    return a;
+}
+int count_B(string s)
+{
+    int b = 0;
+    for(int i = 0; i < s.length(); i++)
+    {
+        if(s[i] == 'b')
+            b++;
+    }
+    
+    return b;
+}
+int count_C(string s)
+{
+    int c = 0;
+    for(int i = 0; i < s.length(); i++)
+    {
+        if(s[i] == 'c')
+            c++;
+    }
+    
+    return c;
+}
+//------------------------------------------
+int main() {
+    int T;
+    string s;
+    cin>>T;
+    while(T)
+    {
+        cin >> s;
+        bool check_s = check_same(s);
+        if(check_s)
+            cout << s.length() << endl;
+        else
+        {
+            
+            int a_num = count_A(s);
+            int b_num = count_B(s);
+            int c_num = count_C(s);
+            
+            bool check_a = ((a_num % 2) == 1); //if zero even;true;
+            bool check_b = ((b_num % 2) == 1); //if zero even;true;
+            bool check_c = ((c_num % 2) == 1); //if zero even;true;
+            bool odd_    = ((check_a) && (check_b) && (check_c));
+            
+                 check_a = ((a_num % 2) == 0); //if zero even;true;
+                 check_b = ((b_num % 2) == 0); //if zero even;true;
+                 check_c = ((c_num % 2) == 0); //if zero even;true;
+            bool even_    = ((check_a) && (check_b) && (check_c));
+            
+            if(odd_ || even_)
+                cout << "2" << endl;
+            else
+                cout << "1" << endl;
+        }
+    
+        T--;
+    }
+    
+    return 0;
+}
