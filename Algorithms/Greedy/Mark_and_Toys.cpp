4c3629e19f3796b829e18f36ddeadc085bc8ad93
Jayteare <Jayteare@users.noreply.github.com>
2015-10-30 23:37:35 -0700

    Create Mark_and_Toys.cpp

---
+++ Algorithms/Greedy/Mark_and_Toys.cpp
@@ -0,0 +1,44 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+#define REP(i,a,b) \
+    for(int i = int(a); i < int(b); i++)
+
+
+int main() 
+{
+    long long n, k; cin >> n >> k;
+    vector<int> prices;
+    for(int i = 0; i < n; i++)
+    {
+        int p; cin >> p;
+        prices.push_back(p);
+    }
+    
+    int answer = 0;
+    int count  = 0;
+    
+    // Write the code for computing the final answer using n,k,prices
+    sort(prices.begin(), prices.end());
+    vector<int>::iterator low; low = std::lower_bound (prices.begin(), prices.end(), k);
+    
+    int pos_ = (low- prices.begin());
+    prices.resize(pos_);
+    
+    REP(i,0,prices.size())
+    {
+        answer += prices.at(i);
+        if(answer > k)
+            break;
+        count++;
+    }
+    
+    cout << count << endl;
+    return 0;
+}
