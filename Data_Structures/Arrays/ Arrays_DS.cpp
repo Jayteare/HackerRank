e24bed9857c50f34f305e92eae2144c11a77d72f
Jayteare <Jayteare@users.noreply.github.com>
2016-10-06 12:11:11 -0700

    Create  Arrays_DS.cpp

---
+++ Data_Structures/Arrays/ Arrays_DS.cpp
@@ -0,0 +1,23 @@
+#include <limits>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int arr_i = 0;arr_i < n;arr_i++){
+       cin >> arr[arr_i];
+    }
+    
+    reverse(arr.begin(),arr.end());
+    for(int arr_i = 0;arr_i < n;arr_i++){
+    cout << arr[arr_i] << " ";
+    }
+    
+    return 0;
+}
