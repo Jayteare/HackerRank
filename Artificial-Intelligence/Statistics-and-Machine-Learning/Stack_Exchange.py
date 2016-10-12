b8274459e5dbb0078f27ca60cfbd16cdad23ce60
Jayteare <Jayteare@users.noreply.github.com>
2016-10-11 17:26:13 -0700

    Update Stack_Exchange.py

--- Artificial-Intelligence/Statistics-and-Machine-Learning/Stack_Exchange.py
+++ Artificial-Intelligence/Statistics-and-Machine-Learning/Stack_Exchange.py
@@ -14,7 +14,7 @@ with io.open('training.json', encoding='utf-8', errors='ignore') as _file:
 	for line in _file:
 		_data.append(json.loads(line))
 
-#Collect the topics available from the JSON file.
+#Collect the topics,questions, and excerpts available from the JSON file.
 for i in range(1,len(_data)):
 	_topic.append(_data[i]['topic'])
 	_question.append(_data[i]['question'])
@@ -24,8 +24,11 @@ for i in range(1,len(_data)):
 _topics = list(set(_topic))
 
 #Analyze a test string
-_words_question = re.sub("[^\w]"," ",_question[0]).split()
-_words_excerpt  = re.sub("[^\w]"," ",_excerpt[0]).split()
-
+index = -1
+for i in range(0,len(_topic)):
+    _words_question = re.sub("[^\w]"," ",_question[i]).split()
+    _words_excerpt  = re.sub("[^\w]"," ",_excerpt[i]).split()
+    for k in range(0,len(_topics)):
+        if(_topics[k] == _topic[i]):
+            index = k
 print _words_question
-print _words_excerpt
