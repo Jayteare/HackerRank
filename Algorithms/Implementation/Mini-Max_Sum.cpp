56946fa05d4602fba422de73057b7184396417cb
Jayteare <Jayteare@users.noreply.github.com>
2016-11-29 17:00:13 -0800

    Create Mini-Max_Sum.cpp

---
+++ Algorithms/Implementation/Mini-Max_Sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+bool size_check(int _size);
+bool size_check(int _size)
+{    
+  if(_size ==5) { return true; }    
+  else{ return false; }
+}
+
+int main()
+{    vector<long int> keys;    vector<long int> values;    int _size = 5;
+        do    {        int _temp = 0;        cin >> _temp;        keys.push_back(_temp);        _size--;    }while(_size > 0);        bool verify_input = size_check(keys.size());        if(verify_input)    {                       std::sort (keys.begin(),keys.end());        long int _val = 0;        for(int index = 0 ; index < (keys.size()-1);index++)        {            _val += keys.at(index);        }
+        values.push_back(_val);        cout << _val;                _val = 0;                for(int index = 1 ; index < keys.size();index++)        {            _val += keys.at(index);        }                values.push_back(_val);        cout << " " << _val;    }        return 0;}
